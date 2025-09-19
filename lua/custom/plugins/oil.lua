92224d4a445fe476251ddcee6fb9ff1e964ef0b2
Mario Wibisono <kobamkode@gmail.com>
2025-09-19 13:24:54 +0700

    add my fav plugins

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      view_options = {
+        show_hidden = true,
+      },
+    },
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    lazy = false,
+    keys = {
+      { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
+    },
+  },
+}
