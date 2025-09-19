e5b71cd1cb530cb64d44222f1fa272852a145946
Mario Wibisono <kobamkode@gmail.com>
2025-09-19 13:24:54 +0700

    add my fav plugins

---
+++ lua/custom/plugins/companion.lua
@@ -0,0 +1,101 @@
+return {
+  {
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'ravitemer/mcphub.nvim',
+      'ravitemer/codecompanion-history.nvim',
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = 'anthropic',
+        },
+        inline = {
+          adapter = 'anthropic',
+        },
+      },
+      adapters = {
+        http = {
+          env = {
+            api_key = function()
+              return os.getenv 'ANTHROPIC_API_KEY'
+            end,
+          },
+        },
+      },
+      extensions = {
+        mcphub = {
+          callback = 'mcphub.extensions.codecompanion',
+          opts = {
+            make_vars = true,
+            make_slash_commands = true,
+            show_result_in_chat = true,
+          },
+        },
+        history = {
+          enabled = true,
+          opts = {},
+        },
+        vectorcode = {
+          opts = {
+            tool_group = {
+              enabled = true,
+              extras = {},
+              collapse = false, -- whether the individual tools should be shown in the chat
+            },
+            tool_opts = {
+              ['*'] = {},
+              ls = {},
+              vectorise = {},
+              query = {
+                max_num = { chunk = -1, document = -1 },
+                default_num = { chunk = 50, document = 10 },
+                include_stderr = false,
+                use_lsp = false,
+                no_duplicate = true,
+                chunk_mode = false,
+                summarise = {
+                  enabled = false,
+                  adapter = nil,
+                  query_augmented = true,
+                },
+              },
+              files_ls = {},
+              files_rm = {},
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    'ravitemer/mcphub.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    build = 'npm install -g mcp-hub@latest',
+    opts = {},
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = { 'markdown', 'codecompanion' },
+    opts = { enabled = true, preset = 'lazy' },
+  },
+  {
+    'echasnovski/mini.diff',
+    config = function()
+      local diff = require 'mini.diff'
+      diff.setup {
+        source = diff.gen_source.none(),
+      }
+    end,
+  },
+  {
+    'Davidyz/VectorCode',
+    version = '*',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = 'VectorCode',
+  },
+}
